b73799ccf0b618d376d551dd553a25d83b1d113c
yerke_aorus <zat.tyme.su@gmail.com>
2025-05-15 07:11:47 +0500

    deploy conf #14

--- qyzmettap_back/settings/release.py
+++ qyzmettap_back/settings/release.py
@@ -2,3 +2,40 @@ from .base import *
 
 
 DEBUG = False
+
+
+CORS_ALLOW_HEADERS = [
+    'Authorization',
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://qyzmetapp-front.vercel.app',
+    'https://qyzmettap.sky-ddns.kz',
+    'http://localhost:5173',
+    'http://localhost:3000',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://qyzmetapp-front.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000',
+]
